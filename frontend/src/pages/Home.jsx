5b8a2594a9755b5e5b02f2e25c22ca06f16cfe69
Bill Liu <billnyesmail@gmail.com>
2024-07-28 01:22:40 -0400

    Added Option To Sort Reviews Added a dropdown that allows the user to sort by newest, oldest, lowest, and highest rating Updated css and layout to match as well.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,11 +7,30 @@ import "../styles/Home.css";
 function Home() {
   const [reviews, setReviews] = useState([]);
   const [file, setFile] = useState(null);
+  const [sortCriteria, setSortCriteria] = useState("newest");
+  const [sortedReviews, setSortedReviews] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     getReviews();
   }, []);
+
+  useEffect(() => {
+    let sorted = [];
+    if (sortCriteria === "newest") {
+      sorted = [...reviews].sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+    if (sortCriteria === "oldest") {
+      sorted = [...reviews].sort((a, b) => new Date(a.date) - new Date(b.date));
+    }
+    if (sortCriteria === "highest") {
+      sorted = [...reviews].sort((a, b) => b.rating - a.rating);
+    }
+    if (sortCriteria === "lowest") {
+      sorted = [...reviews].sort((a, b) => a.rating - b.rating);
+    }
+    setSortedReviews(sorted);
+  }, [reviews, sortCriteria]);
 
   const getReviews = () => {
     api
@@ -24,7 +43,7 @@ function Home() {
       .catch((err) => alert(`Error While Getting Reviews: \n${err}`));
   };
 
-  const deleteReviews = (id) => {
+  const deleteReview = (id) => {
     api
       .delete(`/api/reviews/delete/${id}/`)
       .then((res) => {
@@ -44,10 +63,8 @@ function Home() {
       alert("Please select a file to upload.");
       return;
     }
-
     const formData = new FormData();
     formData.append("har-file", file);
-
     api
       .post("/api/reviews/process-har-file/", formData)
       .then((res) => {
@@ -62,21 +79,36 @@ function Home() {
     navigate("/logout");
   };
 
+  const handleSortChange = (e) => {
+    setSortCriteria(e.target.value);
+  };
+
   return (
     <div className="container">
       <div className="header-section">
         <h1>{reviews.length} Reviews Displayed:</h1>
-        <button onClick={handleLogout}>Log Out</button>
+        <div>
+          <h2>Sort by: </h2>
+          <select onChange={handleSortChange} value={sortCriteria}>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+            <option value="highest">Highest Rating</option>
+            <option value="lowest">Lowest Rating</option>
+          </select>
+        </div>
       </div>
       <div className="reviews-section">
-        {reviews.map((review) => (
-          <Review review={review} onDelete={deleteReviews} key={review.id} />
+        {sortedReviews.map((review) => (
+          <Review review={review} onDelete={deleteReview} key={review.id} />
         ))}
       </div>
       <div className="footer-section">
-        <h2>Upload The .HAR File Retrieved From Your Airbnb Reviews Page</h2>
-        <input type="file" accept=".har" onChange={handleFileChange} />
-        <button onClick={handleUpload}>Upload</button>
+        <div>
+          <h2>Upload The .HAR File Retrieved From Your Airbnb Reviews Page</h2>
+          <input type="file" accept=".har" onChange={handleFileChange} />
+          <button onClick={handleUpload}>Upload</button>
+        </div>
+        <button onClick={handleLogout}>Log Out</button>
       </div>
     </div>
   );
